b97eb5560159110e8d8522d8ea6f226d5bc459a2
Jacob Levesque <jalevesq@c1r8p5.42quebec.com>
2023-03-28 16:27:36 -0400

    LET'S GO multiple > is finally working. git add . Discover some leak (open fd at exit) with valgrind

--- srcs/Executor.c
+++ srcs/Executor.c
@@ -6,7 +6,7 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 10:39:17 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/03/28 15:14:00 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/03/28 16:18:21 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,37 @@
 void ft_child_pipe(t_child *c, t_token *t, int *fd)
 {
 	int fd2;
+	t_token *tmp;
 
+	tmp = t;
 	fd2 = -1;
 	if (t->next && t->next->type == GREAT)
 	{
-		fd2 = open(t->next->next->str, O_WRONLY | O_TRUNC | O_CREAT, 0640);
-		close(fd2);
+		tmp = tmp->next;
+		if (tmp->next->next && tmp->next->next->type == GREAT)
+		{
+			while(tmp && tmp->type == GREAT)
+			{
+				fd2 = -1;
+				fprintf(stderr, "TEST\n");
+				fd2 = open(tmp->next->str, O_WRONLY | O_TRUNC | O_CREAT, 0640);
+				if (tmp->next && tmp->next->next && tmp->next->type == FILE && tmp->next->next->type == GREAT)
+				{
+					close(fd2);
+					tmp = tmp->next->next;
+				}
+				else
+					break;
+			}	
+		}
+		else
+		{
+			fd2 = open(tmp->next->str, O_WRONLY | O_TRUNC | O_CREAT, 0640);
+		}
 		if (dup2(fd2, STDOUT) == -1)
 			ft_error(1); // temp, bad exit
-		fprintf(stderr, "fd2, STDOUT\n");
+		close(fd2);
+		// fprintf(stderr, "fd2, STDOUT\n");
 	}
 	if (t->prev && t->prev->type == PIPE && t->next && t->next->type == PIPE)
 	{
@@ -31,20 +53,20 @@ void ft_child_pipe(t_child *c, t_token *t, int *fd)
 			ft_error(1); // temp, bad exit
 		if (dup2(fd[c->j], STDOUT) == -1)
 			ft_error(1); // temp, bad exit//
-		fprintf(stderr, "ELSE - fd[%d - 1], STDIN\n", c->j);
-		fprintf(stderr, "ELSE - fd[%d], STDOUT\n", c->j);
+		// fprintf(stderr, "ELSE - fd[%d - 1], STDIN\n", c->j);
+		// fprintf(stderr, "ELSE - fd[%d], STDOUT\n", c->j);
 	}
 	else if (c->i == 0 && t->next && t->next->type == PIPE)
 	{
 		if (dup2(fd[c->j], STDOUT) == -1)
 			ft_error(1); // temp, bad exit
-		fprintf(stderr, "fd[%d], STDOUT\n", c->j);
+		// fprintf(stderr, "fd[%d], STDOUT\n", c->j);
 	}
 	else if (t->prev && t->prev->type == PIPE)
 	{
 		if (dup2(fd[c->j - 1], STDIN) == -1)
 			ft_error(1); // temp, bad exit
-		fprintf(stderr, "fd[%d - 1], STDIN\n", c->j);
+		// fprintf(stderr, "fd[%d - 1], STDIN\n", c->j);
 	}
 
 }
